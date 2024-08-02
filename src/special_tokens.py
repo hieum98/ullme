bf5d6c9dd79efc38461d6e435e1462e96c5a9af5
hieuman <hieuman2708@gmail.com>
2024-08-02 09:20:10 -0700

    init

---
+++ src/special_tokens.py
@@ -0,0 +1,33 @@
+SPECIAL_TOKENS = {
+    't5': {
+        'eos': '</s>',
+    },
+    'llama': {
+        'bos': '<|begin_of_text|>',
+        'user_bos': '<|start_header_id|>user<|end_header_id|>\n\n',
+        'eot': '<|eot_id|>\n',
+        'assistant_bos': '<|start_header_id|>assistant<|end_header_id|>\n\n',
+        'eos': '<|end_of_text|>'
+    },
+    'mistral':{
+        'bos': '<s>',
+        'user_bos': '[INST]',
+        'eot': '',
+        'assistant_bos': '[/INST]',
+        'eos': '</s>'
+    },
+    'phi3': {
+        'bos': '',
+        'user_bos': '',
+        'eot': '',
+        'assistant_bos': '',
+        'eos': ''
+    },
+    'phi':{
+        'bos': '',
+        'user_bos': '<|im_start|>user\n\n',
+        'eot': '\n',
+        'assistant_bos': '<|im_start|>assistant\n\n',
+        'eos': '<|im_end|>'
+    },
+}
